0e9cfb0e1ed3998856813f7f92f2d0ff2a0bb69e
swiftsumner <tanglailin@gmail.com>
2025-05-14 11:03:43 +0000

    1

--- app.py
+++ app.py
@@ -2,8 +2,9 @@
 
 from flask import Flask, request, render_template
 import google.generativeai as genai
+import os
 
-genai.configure(api_key=gemini_key)
+genai.configure(api_key=os.environ["gemini_key"])
 model = genai.GenerativeModel("gemini-2.0-flash")
 
 app = Flask(__name__)
